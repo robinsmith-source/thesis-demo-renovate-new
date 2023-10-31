a266bf7275c77d6ffd36663b8d47e4ef42eae41e
Robin Schmidt <schmidtrobin02@gmail.com>
2023-10-31 21:58:01 +0100

    feat: add endpoints for updating recipes and single recipe steps as well as deleting with full type safety, closes #10

--- src/server/api/routers/recipe.ts
+++ src/server/api/routers/recipe.ts
@@ -7,42 +7,22 @@ import {
 } from "~/server/api/trpc";
 
 export const recipeRouter = createTRPCRouter({
-  get: publicProcedure
-    .input(z.object({ id: z.string().cuid() }))
-    .query(({ input, ctx }) => {
-      return ctx.db.recipe.findFirst({
-        where: { id: input.id },
-        include: {
-          steps: {
-            include: {
-              ingredients: true,
-            },
-          },
-          reviews: {
-            include: {
-              author: true,
-            },
-          },
-          labels: true,
-        },
-      });
-    }),
   create: protectedProcedure
     .input(
       z.object({
         name: z.string().min(1),
-        description: z.string().nullable(),
+        description: z.string().optional(),
         requiredUtensils: z.array(z.string()),
         difficulty: z.enum(["EASY", "MEDIUM", "HARD", "EXPERT"]),
         steps: z.array(
           z.object({
             description: z.string(),
-            duration: z.number().min(0), //may be changed to 1
+            duration: z.number().min(0),
             stepType: z.enum(["PREP", "COOK", "REST"]),
             ingredients: z.array(
               z.object({
-                name: z.string().min(0), //may be changed to 1
-                quantity: z.number().min(0), //may be changed to 1
+                name: z.string().min(1),
+                quantity: z.number().min(1),
                 unit: z.string().optional(),
               }),
             ),
@@ -55,7 +35,7 @@ export const recipeRouter = createTRPCRouter({
         data: {
           name: input.name,
           description: input.name,
-          requiredUtensils: input.requiredUtensils,
+          requiredUtensils: { set: input.requiredUtensils },
           difficulty: input.difficulty,
           steps: {
             createMany: {
@@ -83,14 +63,184 @@ export const recipeRouter = createTRPCRouter({
   getLatest: protectedProcedure.query(({ ctx }) => {
     return ctx.db.recipe.findFirst({
       orderBy: { createdAt: "desc" },
-      where: { author: { id: ctx.session.user.id } },
+      where: {
+        author: {
+          id: ctx.session.user.id,
+        },
+      },
+      include: {
+        steps: {
+          include: {
+            ingredients: true,
+          },
+        },
+        reviews: {
+          include: {
+            author: true,
+          },
+        },
+        labels: true,
+      },
     });
   }),
 
   getAll: protectedProcedure.query(({ ctx }) => {
     return ctx.db.recipe.findMany({
       orderBy: { createdAt: "desc" },
-      where: { author: { id: ctx.session.user.id } },
+      where: {
+        author: {
+          id: ctx.session.user.id,
+        },
+      },
+      include: {
+        steps: {
+          include: {
+            ingredients: true,
+          },
+        },
+        reviews: {
+          include: {
+            author: true,
+          },
+        },
+        labels: true,
+      },
     });
   }),
+
+  get: publicProcedure
+    .input(z.object({ id: z.string().cuid() }))
+    .query(({ input, ctx }) => {
+      return ctx.db.recipe.findFirst({
+        where: { id: input.id },
+        include: {
+          steps: {
+            include: {
+              ingredients: true,
+            },
+          },
+          reviews: {
+            include: {
+              author: true,
+            },
+          },
+          labels: true,
+        },
+      });
+    }),
+
+  updateRecipe: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().cuid(),
+        name: z.string().min(1),
+        description: z.string().optional(),
+        requiredUtensils: z.array(z.string()),
+        difficulty: z.enum(["EASY", "MEDIUM", "HARD", "EXPERT"]),
+        steps: z.array(
+          z.object({
+            description: z.string(),
+            duration: z.number().min(0),
+            stepType: z.enum(["PREP", "COOK", "REST"]),
+            ingredients: z.array(
+              z.object({
+                name: z.string().min(1),
+                quantity: z.number().min(1),
+                unit: z.string().optional(),
+              }),
+            ),
+          }),
+        ),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.recipe.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          name: input.name,
+          description: input.description,
+          requiredUtensils: { set: input.requiredUtensils },
+          difficulty: input.difficulty,
+          steps: {
+            updateMany: {
+              where: {},
+              data: input.steps.map((step) => ({
+                description: step.description,
+                duration: step.duration,
+                stepType: step.stepType,
+                ingredients: {
+                  updateMany: {
+                    where: {},
+                    data: step.ingredients.map((ingredient) => ({
+                      name: ingredient.name,
+                      quantity: ingredient.quantity,
+                      unit: ingredient.unit,
+                    })),
+                  },
+                },
+              })),
+            },
+          },
+        },
+      });
+    }),
+
+  updateRecipeStep: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().cuid(),
+        stepId: z.string().cuid(),
+        description: z.string(),
+        duration: z.number().min(0),
+        stepType: z.enum(["PREP", "COOK", "REST"]),
+        ingredients: z.array(
+          z.object({
+            name: z.string().min(1),
+            quantity: z.number().min(1),
+            unit: z.string().optional(),
+          }),
+        ),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.recipe.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          steps: {
+            update: {
+              where: {
+                id: input.id,
+              },
+              data: {
+                description: input.description,
+                duration: input.duration,
+                stepType: input.stepType,
+                ingredients: {
+                  updateMany: {
+                    where: {},
+                    data: input.ingredients.map((ingredient) => ({
+                      name: ingredient.name,
+                      quantity: ingredient.quantity,
+                      unit: ingredient.unit,
+                    })),
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+    }),
+
+  deleteRecipe: protectedProcedure
+    .input(z.object({ id: z.string().cuid() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.recipe.delete({
+        where: { id: input.id },
+      });
+    }),
 });
