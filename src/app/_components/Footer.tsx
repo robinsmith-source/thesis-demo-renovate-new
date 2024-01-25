1eac9f197595a7dc91eef0ffbd48bc41ad259642
Tobias Messner <tobias@zusor.io>
2024-01-25 16:06:29 +0100

    fix: follow style guide

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,11 +1,7 @@
-// Footer.js
-
-const Footer = () => {
+export default function Footer() {
   return (
     <footer className="flex justify-center bg-primary-400/30 p-4">
       <p className="text-lg font-medium">© 2023 Goose Chef</p>
     </footer>
   );
 };
-
-export default Footer;
